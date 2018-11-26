06280868d9d720c10e0e2a9e19421bc9608674ab
ratsimisampy <ratsimisampy@gmail.com>
2018-11-27 00:40:10 +0100

    fix item show

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,6 +1,6 @@
 class ItemController < ApplicationController
     def show
         puts "Heyy Item show"
-        @potin = @potin.find(params[:id])
+        @item = @item.find(params[:id])
     end
 end
