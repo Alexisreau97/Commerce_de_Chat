49ee6ac52b07b9bf4b6eb982e9b5f674a9e93e77
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-26 17:47:21 +0100

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |item|
+  items = Item.create!(title: Faker::Cat.name, description: Faker::Lorem.paragraph, price: Faker::Commerce.price ,image_url: Faker::Avatar.image)
+end
