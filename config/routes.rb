caf6a6851e584266d6677f71d8c710c39798890e
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-27 16:48:23 +0100

    add form style

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index3'
   devise_for :users
-
   resources :items
-
   resources :charges, only: [:new, :create]
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
