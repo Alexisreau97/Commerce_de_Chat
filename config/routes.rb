0fced2fb48eea8c75f6e7538539d0636bda61087
Alexisreau97 <alexis.reaupro@gmail.com>
2018-11-29 15:35:56 +0100

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :line_items
-  resources :carts
   root 'home#index'
   devise_for :users
-  resources :items
+  resources :items, :line_items, :orders, :carts
   resources :charges, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
